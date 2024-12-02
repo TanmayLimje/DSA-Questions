b32a5dc876ebf88807b93bb240607643c064dc27
Tanmay Limje <103094622+TanmayLimje@users.noreply.github.com>
2024-12-02 14:50:59 +0530

    Update Two_sum.java

--- Two_sum.java
+++ Two_sum.java
@@ -1,6 +1,8 @@
+import java.util.HashMap;
+
 public class Two_sum {
 
-    public static int[] two_sum(int[] nums, int target){
+    public static int[] two_sum_brute(int[] nums, int target){
         for(int p1 = 0; p1 < nums.length; p1++){
             int toFind = target - nums[p1];
 
@@ -10,12 +12,28 @@ public class Two_sum {
                 }
             }
         }
-        return new int[] {-1,-1};
+        return null;
+    }
+
+    public static int[] two_sum_optimal(int [] nums, int target){
+        HashMap<Integer,Integer> numsMap = new HashMap<>();
+
+        for(int p = 0; p < nums.length; p++){
+            if(numsMap.containsKey(nums[p])){
+                return new int[] { numsMap.get(nums[p]),p };
+            }
+            else{
+                int toFind = target - nums[p];
+                numsMap.put(toFind,p);
+            }
+        }
+
+        return null;
     }
 
     public static void main(String [] arg){
-        int[] nums = new int[] {1,7,11,15};
-        int target = 9;
+        int[] nums = new int[] {1,3,7,9,2};
+        int target = 11;
 
         System.out.println("----------TWO SUM----------");
         System.out.println("input array");
@@ -23,8 +41,13 @@ public class Two_sum {
             System.out.print(nums[i]);
             System.out.print(" ");
         }
-        int[] result = two_sum(nums, target);
-        System.out.println("\nAnswer: " + result[0] + " " + result[1]);
+        int[] result = two_sum_optimal(nums, target);
+
+        if (result != null) {
+            System.out.println("\nIndices: [" + result[0] + ", " + result[1] + "]");
+        } else {
+            System.out.println("\nNo solution found");
+        }
     }
 
 }
